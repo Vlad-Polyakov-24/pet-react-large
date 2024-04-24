606ac9c04c826d8756fe5435fb2109e6824d862b
Vladyslav Poliakov <vladon24121995@gmail.com>
2024-04-24 12:31:07 +0300

    feat: Webpack Bundle Analyzer

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -2,6 +2,8 @@ import webpack from 'webpack';
 import HTMLWebpackPlugin from 'html-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import { BuildOptions } from './types/config';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
+
 
 export const buildPlugins = ({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] => [
     new HTMLWebpackPlugin({
@@ -16,4 +18,7 @@ export const buildPlugins = ({ paths, isDev }: BuildOptions): webpack.WebpackPlu
         __IS_DEV__: JSON.stringify(isDev),
     }),
     new webpack.HotModuleReplacementPlugin(),
+    new BundleAnalyzerPlugin({
+        openAnalyzer: false,
+    }),
 ];
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,3 @@
-import { Suspense } from 'react';
 import './styles/index.scss';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { classNames } from 'shared/lib/classNames/classNames';
@@ -11,13 +10,11 @@ const App = () => {
 
 	return (
 		<div className={classNames('app', {}, [theme])}>
-			<Suspense>
-				<Header/>
-				<div className='page'>
-					<Sidebar/>
-					<AppRouter/>
-				</div>
-			</Suspense>
+			<Header/>
+			<div className='page'>
+				<Sidebar/>
+				<AppRouter/>
+			</div>
 		</div>
 	);
 };
